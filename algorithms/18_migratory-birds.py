2c8de55f93f6053e4a34711e452465a385ddef94
yashprsin <yaaahko@gmail.com>
2023-12-03 03:52:31 +0530

    update

---
+++ algorithms/18_migratory-birds.py
@@ -0,0 +1,34 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'migratoryBirds' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts INTEGER_ARRAY arr as parameter.
+#
+
+def migratoryBirds(arr):
+    # Write your code here
+    ls = [0,0,0,0,0,0]
+    for i in range(len(arr)):
+        ls[arr[i]] +=1
+    return ls.index(max(ls))
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    arr_count = int(input().strip())
+
+    arr = list(map(int, input().rstrip().split()))
+
+    result = migratoryBirds(arr)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
